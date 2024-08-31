bdb2c1e48f4e2e123a5920057393e03ea9ff11e2
waterchick <wolfsfromhell@gmail.com>
2024-08-31 21:42:31 +0000

    Edit locales

--- resources/js/locales/cs.ts
+++ resources/js/locales/cs.ts
@@ -10,10 +10,10 @@ export default {
     plugins: 'Mé Pluginy',
     pluginsDesc: 'Pluginy jsou k dispozici na SpigotMC a BuiltByBit. Vytvářím pluginy, které jsou jednoduché, spolehlivé, stabilní a bezplatné.',
     aboutText: 'Ahoj! Jmenuji se Kevin, jsem student a průměrný uživatel Javy. Programování je moje vášeň a neustále se snažím zlepšovat své dovednosti. Specializuji se na vytváření pluginů pro Minecraft Bukkit, BungeeCord a Velocity. Jsem otevřený zakázkám, takže mě neváhejte kontaktovat pro vývoj pluginů na zakázku!',
-    contact: 'Kontaktuj ma',
+    contact: 'Kontaktujte mě',
     contactNameInput: 'Vaše jméno',
     contactEmailInput: 'Emailová adresa',
     contactMessageInput: 'Zpráva',
-    contactDesc: 'Budem rad, ak sa mi ozvete. Vyplňte prosím nasledujúci formulár a kontaktuj ma.',
+    contactDesc: 'Budu rád, pokud se mi ozvete. Vyplňte prosím nasledujúci formulář.',
     contactBtn: 'Odeslat'
 }
